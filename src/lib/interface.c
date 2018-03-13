02467c4dce85b812e5a9562729ed71682f6900ad
Ricardo Caçador <ricardo.fsc11@gmail.com>
2018-03-13 17:04:57 +0000

    struct quase

--- src/lib/interface.c
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-
-typedef struct TCD_community{
-	int n_utilizador,pontos_reputacao,votacoes;
-	String respostas[],tags[];
-	struct nodoTCD_community *esq, * dir;
-}TCD_community* pergunta;
-
