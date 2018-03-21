eda12caa541c3e95032794ed929c4c0660d0866b
joaop21 <jpsilva9898@gmail.com>
2018-03-21 17:05:57 +0000

    init

--- src/lib/init.c
+++ src/lib/init.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "struct.c"
 #include "date.h"
 #include "user.h"
@@ -6,21 +7,21 @@
 // o user e o date sao inicializados segundo os metodos dados no .c pq são tipos abstratos
 
 TAD_community init(){
-	TAD_community tad = NULL;
-
+	TAD_community tad = malloc(sizeof(struct TCD_community));
 	tad->utilizador = malloc(sizeof(struct utilizador)*700); // inicializei com 700000 podia ser com outro qqr
 	tad->n_utilizadores = 0;
-
+	
 	for(int i = 0 ; i!=700 ; i++){
-		tad->utilizador[i]->nome = NULL;
+		tad->utilizador[i] = malloc(sizeof(struct utilizador));
+		tad->utilizador[i]->nome = NULL; ////////
 		tad->utilizador[i]->id = NULL;
 		long array[10] = {0};
 		tad->utilizador[i]->user = create_user(NULL,array);
 		tad->utilizador[i]->reputacao = NULL;
 		tad->utilizador[i]->n_posts = 0;
-		
 		tad->utilizador[i]->posts = malloc(sizeof(struct posts)*10);
 		for(int j = 0 ; j != 10 ; j++){
+			tad->utilizador[i]->posts[j] = malloc(sizeof(struct posts));
 			tad->utilizador[i]->posts[j]->data = createDate(0,0,0);
 			tad->utilizador[i]->posts[j]->id_post = NULL;
 			tad->utilizador[i]->posts[j]->score = NULL;
@@ -30,6 +31,7 @@ TAD_community init(){
 			
 			tad->utilizador[i]->posts[j]->tags = malloc(sizeof(10));
 				for(int k = 0 ; k != 10 ; k++){
+					tad->utilizador[i]->posts[j]->tags[k] = malloc(sizeof(1));
 					tad->utilizador[i]->posts[j]->tags[k] = NULL;
 				}
 		}
--- src/main.c
+++ src/main.c
@@ -3,7 +3,11 @@
 
 int main(){
   TAD_community tda = NULL;
+  
+  printf("................Ainda não dá erro...............\n");
+  
   tda = init();
+  
   printf("Done.. grande estrutura parceiros.\n");
   return 0;
 } 
