9c2f24bba2df1c6996a0298a267b8006995e2564
joaop21 <jpsilva9898@gmail.com>
2018-03-26 16:31:59 +0100

    main iterativo xd

--- src/main.c
+++ src/main.c
@@ -5,20 +5,83 @@
 int main(){
   TAD_community tda = NULL;
   STR_pair par = create_str_pair(NULL,NULL);
-  LONG_list l = create_list(5);
+  LONG_list l = create_list(10);
+  int menu = 0;
   
   tda = init();
-  printf("A estrutura já foi inicializada...\n");
+  printf("[init.c] Estrutura já foi inicializada...\n");
 
   char* path1 = "../../dumpexemplo/askubuntu.com/";
   char* path2 = "../../dumpexemplo/android/";
   tda = load(tda, path2);
 
-  //par = info_from_post(tda,4567);
-  l = top_most_active(tda,5);
+  while(menu == 0){
+    printf("Que questão deseja resolver? (-1, para sair do programa)\n");
+    scanf("%d", &menu);
+
+    if(menu == 1){
+      printf("Ainda nao está acabada.\n");
+      //par = info_from_post(tda,4567);
+      menu = 0;
+    }
+    if(menu == 2){
+      int tamanho;
+      printf("Qual o número de utilizadores que pretende?\n");
+      scanf("%d",&tamanho);
+      LONG_list l = create_list(tamanho);
+      l = top_most_active(tda,tamanho);
+      menu = 0;
+    }
+    if(menu == 3){
+      printf("Ainda nao está acabada.\n");
+      //par = info_from_post(tda,4567);
+      menu = 0;
+    }
+    if(menu == 4){
+      printf("Ainda nao está acabada.\n");
+      //par = info_from_post(tda,4567);
+      menu = 0;
+    }
+    if(menu == 5){
+      printf("Ainda nao está acabada.\n");
+      //par = info_from_post(tda,4567);
+      menu = 0;
+    }
+    if(menu == 6){
+      printf("Ainda nao está acabada.\n");
+      //par = info_from_post(tda,4567);
+      menu = 0;
+    }
+    if(menu == 7){
+      printf("Ainda nao está acabada.\n");
+      //par = info_from_post(tda,4567);
+      menu = 0;
+    }
+    if(menu == 8){
+      printf("Ainda nao está acabada.\n");
+      //par = info_from_post(tda,4567);
+      menu = 0;
+    }
+    if(menu == 9){
+      printf("Ainda nao está acabada.\n");
+      //par = info_from_post(tda,4567);
+      menu = 0;
+    }
+    if(menu == 10){
+      printf("Ainda nao está acabada.\n");
+      //par = info_from_post(tda,4567);
+      menu = 0;
+    }
+    if(menu == 11){
+      printf("Ainda nao está acabada.\n");
+      //par = info_from_post(tda,4567);
+      menu = 0;
+    }
+    if(menu==-1) break;
+  }
 
   free(tda);
   
-  printf("Done.. grande load.\n");
+  printf("Program Done...\n");
   return 0;
 } 
