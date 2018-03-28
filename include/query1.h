1202a1111896d535cc49546e2119c39546dad2f8
joaop21 <jpsilva9898@gmail.com>
2018-03-28 02:13:57 +0100

    query3 done

--- include/query1.h
+++ include/query1.h
@@ -1,10 +1,6 @@
 #ifndef __QUERY1__
 #define __QUERY1__
 
-int procura_binaria_u (TAD_community com, int id, int Tam);
-
-int procura_binaria_p (TAD_community com, int id, int Tam);
-
 STR_pair info_from_post(TAD_community com, int id);
 
 #endif
--- src/lib/query3.c
+++ src/lib/query3.c
@@ -20,7 +20,9 @@ LONG_pair total_posts(TAD_community com, Date begin, Date end){
 			else set_snd_long(par,get_snd_long(par)+1); // se for resposta
 		}
 	}
-
+	/*
+	printf("perguntas: %ld\n", get_fst_long(par));
+	printf("respostas: %ld\n", get_snd_long(par));*/
 	return par;
 }
 
@@ -28,25 +30,31 @@ LONG_pair total_posts(TAD_community com, Date begin, Date end){
 int difDatas(Date x,Date begin, Date end) { // 0 ou -1 se está entre as datas ou n
 	int inicio = 0,fim;
 	if(get_year(x) < get_year(begin)) inicio=-1;
-	else if (get_year(x) == get_year(begin)) {
-
-		if (get_month(x) < get_month(begin)) inicio=-1;
-		else if (get_month(x) == get_month(begin)) {
-
-			if (get_day(x) < get_day(begin)) inicio= -1;
-			else inicio= 0;
+	else {
+		if (get_year(x) == get_year(begin)) {
+			if (get_month(x) < get_month(begin)) inicio=-1;
+			else {
+				if (get_month(x) == get_month(begin)) {
+					if (get_day(x) < get_day(begin)) inicio= -1;
+					else inicio= 0;
+				}
+				else inicio = 0;
+			}
 		}
 		else inicio = 0;
 	}
 
 	if(get_year(x) > get_year(end)) fim=-1;
-	else if (get_year(x) == get_year(end)) {
-
-		if (get_month(x) > get_month(end)) fim =-1;
-		else if (get_month(x) == get_month(end)) {
-
-			if (get_day(x) > get_day(end)) fim = -1;
-			else fim = 0;
+	else {
+		if (get_year(x) == get_year(end)) {
+			if (get_month(x) > get_month(end)) fim =-1;
+			else {
+				if (get_month(x) == get_month(end)) {
+					if (get_day(x) > get_day(end)) fim = -1;
+					else fim = 0;
+				}
+				else fim = 0;
+			}
 		}
 		else fim = 0;
 	}
