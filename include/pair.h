699fd8c2e96d1dfc6ba9344c788c319d64eb4f8f
Ricardo Caçador <ricardo.fsc11@gmail.com>
2018-04-27 18:41:09 +0100

    documentação

--- include/pair.h
+++ include/pair.h
@@ -10,31 +10,126 @@ typedef struct str_pair* STR_pair;
 
 
 /**
-\brief Função que recebe um inteiro e cria uma Long List com esse tamanho.
+\brief Função que recebe 2 strings e cria um par de strings.
 
-@param size Tamanho da lista.
+@param fst Primeira string.
+@param snd Segunda string.
 
-@returns Long List com o tamanho desejado.
+@returns Um par de strings.
 */
 
 STR_pair create_str_pair(char* fst, char* snd);
 
+/**
+\brief Função que recebe um STR_pair e uma string e coloca na primeira posição a string recebida.
 
+@param pair Par de strings.
+@param str String a colocar.
+
+*/
 
 void set_fst_str(STR_pair pair, char* str);
+
+/**
+\brief Função que recebe um STR_pair e uma string e coloca na segunda posição a string recebida.
+
+@param pair Par de strings.
+@param str String a colocar.
+
+*/
+
 void set_snd_str(STR_pair pair, char* str);
+
+/**
+\brief Função que recebe um STR_pair e devolve a primeira string.
+
+@param pair Par de strings.
+
+@returns String da primeira posição.
+*/
+
+
 char* get_fst_str(STR_pair pair);
+
+/**
+\brief Função que recebe um STR_pair e devolve a segunda string.
+
+@param pair Par de strings.
+
+@returns String da segunda posição.
+*/
+
 char* get_snd_str(STR_pair pair);
+
+/**
+\brief Função que recebe um STR_pair e liberta a memória alocada por esta.
+
+@param pair Par de strings.
+*/
+
 void free_str_pair(STR_pair pair);
 
 // LONG_pair definitions
 typedef struct long_pair* LONG_pair;
 
+/**
+\brief Função que recebe 2 long's e cria um par.
+
+@param fst Primeiro long.
+@param snd Segunda long.
+
+@returns Um par de long's.
+*/
+
+
 LONG_pair create_long_pair(long fst, long snd);
+
+/**
+\brief Função que recebe um LONG_pair e um long e coloca na primeira posição o long recebido.
+
+@param pair Par de long's.
+@param l Long a colocar.
+
+*/
+
 void set_fst_long(LONG_pair pair, long l);
+
+/**
+\brief Função que recebe um LONG_pair e um long e coloca na segunda posição o long recebido.
+
+@param pair Par de long's.
+@param l Long a colocar.
+
+*/
+
 void set_snd_long(LONG_pair pair, long l);
+
+/**
+\brief Função que recebe um LONG_pair e devolve a primeira posição.
+
+@param pair Par de long's.
+
+@returns Long da primeira posição.
+*/
+
 long get_fst_long(LONG_pair pair);
+
+/**
+\brief Função que recebe um LONG_pair e devolve a segunda posição.
+
+@param pair Par de long's.
+
+@returns Long da segunda posição.
+*/
+
 long get_snd_long(LONG_pair pair);
+
+/**
+\brief Função que recebe um LONG_pair e liberta a memória alocada por este.
+
+@param pair Par de long's.
+*/
+
 void free_long_pair(LONG_pair pair);
 
 #endif
--- include/posts.h
+++ include/posts.h
@@ -1,34 +1,253 @@
 #ifndef __POSTS__
 #define __POSTS__
 
+/**
+@file posts.h
+Posts do projeto.
+*/
+
 #include "date.h"
 #include "common.h"
 #include <glib.h>
 
 typedef struct posts* POSTS;
 
+
+/**
+@brief Função que cria uma estrutura POSTS.
+
+@returns Estrutura inicializada de POSTS.
+*/
+
 POSTS create_posts();
+
+/**
+@brief Função que recebe uma estrutura POSTS e retorna a key_id_post.
+
+@param p Estrutura POSTS.
+
+@returns key_id_post da estrutura recebida.
+*/
+
 long get_key_id_post(POSTS p);
+
+/**
+@brief Função que recebe uma estrutura POSTS e retorna a Data em formato Date.
+
+@param p Estrutura POSTS.
+
+@returns Data da estrutura recebida.
+*/
+
 Date get_data(POSTS p);
+
+/**
+@brief Função que recebe uma estrutura POSTS e retorna a data em formato gchar*.
+
+@param p Estrutura POSTS.
+
+@returns Data da estrutura recebida.
+*/
+
 gchar* get_data_string(POSTS p);
+
+/**
+@brief Função que recebe uma estrutura POSTS e retorna o score do post.
+
+@param p Estrutura POSTS.
+
+@returns Score do post.
+*/
+
 long get_score(POSTS p);
+
+/**
+@brief Função que recebe uma estrutura POSTS e retorna o ID do utilizador que fez o post.
+
+@param p Estrutura POSTS.
+
+@returns ID do utilizador.
+*/
+
 long get_owner_user_id(POSTS p);
+
+/**
+@brief Função que recebe uma estrutura POSTS e retorna o título do post.
+
+@param p Estrutura POSTS.
+
+@returns Título do post.
+*/
+
 gchar* get_title(POSTS p);
+
+/**
+@brief Função que recebe uma estrutura POSTS e retorna o corpo da pergunta.
+
+@param p Estrutura POSTS.
+
+@returns Corpo da pergunta.
+*/
+
 gchar* get_body(POSTS p);
+
+/**
+@brief Função que recebe uma estrutura POSTS e retorna o tipo de post.
+
+@param p Estrutura POSTS.
+
+@returns Tipo do post recebido.
+*/
+
 int get_post_type_id(POSTS p);
+
+/**
+@brief Função que recebe uma estrutura POSTS e retorna o ID do pai, ou seja da pergunta à qual corresponde.
+
+@param p Estrutura POSTS.
+
+@returns ID da pergunta a que se refere.
+*/
+
 long get_parent_id(POSTS p);
+
+/**
+@brief Função que recebe uma estrutura POSTS e retorna uma lista com as Tags da pergunta.
+
+@param p Estrutura POSTS.
+
+@returns Lista das tags.
+*/
+
 GList* get_tags(POSTS p);
+<<<<<<< Updated upstream
+=======
+
+/**
+@brief Função que recebe uma estrutura POSTS e retorna o número de respostas à pergunta.
+
+@param p Estrutura POSTS.
+
+@returns Número de respostas da pergunta.
+*/
+
+long get_answer_count(POSTS p);
+
+/**
+@brief Função que recebe uma estrutura POSTS e retorna o número de comentários.
+
+@param p Estrutura POSTS.
+
+@returns Nº de comentários da resposta.
+*/
+
+>>>>>>> Stashed changes
 long get_comment_count(POSTS p);
+
+/**
+@brief Função que recebe uma estrutura POSTS e coloca uma key_id_post.
+
+@param p Estrutura POSTS.
+*/
+
 void set_key_id_post(POSTS p, int key_id_post);
+
+/**
+@brief Função que recebe uma estrutura POSTS e coloca uma data em formato Date.
+
+@param p Estrutura POSTS.
+*/
+
 void set_data(POSTS p, Date data);
+
+/**
+@brief Função que recebe uma estrutura POSTS e coloca uma data em formato String.
+
+@param p Estrutura POSTS.
+*/
+
 void set_data_string(POSTS p, char* str);
+
+/**
+@brief Função que recebe uma estrutura POSTS e coloca um score.
+
+@param p Estrutura POSTS.
+*/
+
 void set_score(POSTS p, int score);
+
+/**
+@brief Função que recebe uma estrutura POSTS e coloca um ID de utilizador.
+
+@param p Estrutura POSTS.
+*/
+
 void set_owner_user_id(POSTS p, int owner_user_id);
+
+/**
+@brief Função que recebe uma estrutura POSTS e coloca um título.
+
+@param p Estrutura POSTS.
+*/
+
 void set_title(POSTS p, char* str);
+
+/**
+@brief Função que recebe uma estrutura POSTS e coloca um corpo de texto.
+
+@param p Estrutura POSTS.
+*/
+
 void set_body(POSTS p, char* str);
+
+/**
+@brief Função que recebe uma estrutura POSTS e coloca o post_type_id.
+
+@param p Estrutura POSTS.
+*/
+
 void set_post_type_id(POSTS p, int post_type_id);
+
+/**
+@brief Função que recebe uma estrutura POSTS e coloca o ID do pai do post.
+
+@param p Estrutura POSTS.
+*/
+
 void set_parent_id(POSTS p, int parent_id);
+
+/**
+@brief Função que recebe uma estrutura POSTS e coloca as tags da pergunta.
+
+@param p Estrutura POSTS.
+*/
+
 void set_tags(POSTS p, char* str);
+<<<<<<< Updated upstream
+=======
+
+/**
+@brief Função que recebe uma estrutura POSTS e coloca o número de respostas.
+
+@param p Estrutura POSTS.
+*/
+
+void set_answer_count(POSTS p, int answer_count);
+
+/**
+@brief Função que recebe uma estrutura POSTS e coloca o número de comentários.
+
+@param p Estrutura POSTS.
+*/
+
+>>>>>>> Stashed changes
 void set_comment_count(POSTS p, int comment_count);
+
+/**
+@brief Função que liberta a memória alocada por uma estrutura POSTS.
+
+@param p Estrutura POSTS.
+*/
+
 void free_posts(POSTS p);
 #endif
