cbcfb021401ee04ea52706e7de0a0e64aa9aedc8
joaop21 <jpsilva9898@gmail.com>
2018-04-28 00:13:59 +0100

    documentação pendente

--- include/answer_count.h
+++ include/answer_count.h
@@ -1,15 +1,71 @@
 #ifndef __ANSWER_COUNT__
 #define __ANSWER_COUNT__
 
+/**
+@file answer_count.h
+Contém uma pequena estrutura auxiliar denominada de answer_count.
+*/
+
 #include <glib.h>
 
 typedef struct answer_count * ANSWER_COUNT;
 
+/**
+@brief Função que cria uma estrutura ANSWER_COUNT.
+
+@returns Estrutura inicializada de ANSWER_COUNT.
+*/
+
 ANSWER_COUNT create_answer_count();
+
+/**
+@brief Função que recebe uma estrutura ANSWER_COUNT e retorna a o número de resposta associado.
+
+@param t Estrutura ANSWER_COUNT.
+
+@returns Um long associado ao número de respostas.
+*/
+
 long get_num_respostas(ANSWER_COUNT t);
+
+/**
+@brief Função que recebe uma estrutura ANSWER_COUNT e retorna a o Id da pergunta associado.
+
+@param t Estrutura ANSWER_COUNT.
+
+@returns Um long associado ao número de respostas.
+*/
+
 long get_id_pergunta(ANSWER_COUNT t);
+
+/**
+@brief Função que recebe uma estrutura ANSWER_COUNT e altera o número de respostas associado.
+
+@param t Estrutura ANSWER_COUNT.
+@param incremento Valor a adicionar ao corrente número de respostas.
+
+@returns Um long associado ao número de respostas.
+*/
+
 void set_num_respostas(ANSWER_COUNT t, int incremento);
+
+/**
+@brief Função que recebe uma estrutura ANSWER_COUNT e altera o Id associado.
+
+@param t Estrutura ANSWER_COUNT.
+@param id_l Valor de Id a ser alterado na estrutura.
+
+@returns Um long associado ao número de respostas.
+*/
+
 void set_id_pergunta(ANSWER_COUNT t, long id_l);
+
+/**
+@brief Função que liberta a memória alocada por uma estrutura ANSWER_COUNT.
+
+@param t Estrutura ANSWER_COUNT.
+*/
+
 void free_answer_count(ANSWER_COUNT t);
 
 #endif
