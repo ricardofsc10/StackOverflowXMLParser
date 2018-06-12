779799b191475432c1697e0aa7f23d22564f25a6
joaop21 <jpsilva9898@gmail.com>
2018-06-12 16:07:48 +0100

    erros

--- proj-java/src/main/java/li3/Main.java
+++ proj-java/src/main/java/li3/Main.java
@@ -1,15 +1,7 @@
 package li3;
 
-import common.MyLog;
-import common.Pair;
 import engine.*;
-import org.xml.sax.SAXException;
 
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.IOException;
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
 
 public class Main {
 
